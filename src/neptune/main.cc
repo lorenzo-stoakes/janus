14cae59684cc9d8b3f0e2643475dc882771f6001
Lorenzo Stoakes <lstoakes@gmail.com>
2020-04-01 17:51:19 +0100

    neptune: Add a description and inital cmdline processing
    Give a brief overview of what the tool is intended to be for reference.

--- src/neptune/main.cc
+++ src/neptune/main.cc
@@ -2,7 +2,19 @@
 
 #include <iostream>
 
+// Neptune is a tool for converting existing JSON files to a binary format for
+// processing by janus.
+
+// In its initial, skeleton, implementation the tool will process only meta JSON
+// files.
+
 auto main(int argc, char** argv) -> int
 {
+	if (argc < 3) {
+		std::cerr << "usage: " << argv[0] << // NOLINT
+			" [output file] [meta json file(s)]..." << std::endl;
+		return 1;
+	}
+
 	return 0;
 }
