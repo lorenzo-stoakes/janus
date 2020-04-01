49c69a3b3e9eb82ff38c642ae0e3e74bdb6ac7c8
Lorenzo Stoakes <lstoakes@gmail.com>
2020-04-01 12:32:14 +0100

    dynamic_buffer: style: Linter fixes
    Fix reports from static analysis tools.

--- include/dynamic_buffer.hh
+++ include/dynamic_buffer.hh
@@ -18,16 +18,16 @@ public:
 		// Note that we store capacity/size values in uint64 words.
 		: _cap{align64(cap) / sizeof(uint64_t)},
 		  _size{0},
-		  _buf{std::make_unique<uint64_t[]>(_cap)}
+		  _buf{std::make_unique<uint64_t[]>(_cap)} // NOLINT: Can't use std::array here.
 	{
 	}
 
 	// Make the buffer moveable.
-	dynamic_buffer(dynamic_buffer&& that)
+	dynamic_buffer(dynamic_buffer&& that) noexcept
 		: _cap{that._cap}, _size{that._size}, _buf{std::move(that._buf)}
 	{
 		that._cap = 0;
-		that._buf = 0;
+		that._buf = nullptr;
 	}
 
 	// The default dtor is fine, our underlying buffer is wrapped by a unique_ptr.
@@ -35,14 +35,14 @@ public:
 
 	// Delete the copy ctor + assignment operator we want to discourage copying.
 	dynamic_buffer(const dynamic_buffer&) = delete;
-	dynamic_buffer& operator=(const dynamic_buffer&) = delete;
+	auto operator=(const dynamic_buffer&) -> dynamic_buffer& = delete;
 	// Discourage move assignment as well, we don't want to be overwriting
 	// a buffer.
-	dynamic_buffer& operator=(dynamic_buffer&&) = delete;
+	auto operator=(dynamic_buffer &&) -> dynamic_buffer& = delete;
 
 	// Overall maximum capacity of the buffer in bytes. If this is exceeded, we
 	// throw.
-	auto cap() -> uint64_t const
+	auto cap() const -> uint64_t
 	{
 		// We store this value as a multiple of uint64_t words
 		// internally.
@@ -51,7 +51,7 @@ public:
 
 	// Current used bytes, also represents the offset at which data will be
 	// written. size() <= cap() is an invariant.
-	auto size() -> uint64_t const
+	auto size() const -> uint64_t
 	{
 		// We store this value as a multiple of uint64_t words
 		// internally.
@@ -59,7 +59,7 @@ public:
 	}
 
 	// Return raw underlying buffer.
-	auto data() -> const uint64_t* const
+	auto data() const -> const uint64_t*
 	{
 		return _buf.get();
 	}
@@ -76,18 +76,18 @@ public:
 	//      ptr: Raw pointer of memory to copy to buffer.
 	//     size: Size in bytes of memory to copy.
 	//  returns: Raw pointer to allocated memory.
-	void* add_raw(void* ptr, uint64_t size)
+	auto add_raw(void* ptr, uint64_t size) -> void*
 	{
 		uint64_t aligned_bytes = align64(size);
 		uint64_t aligned_words = aligned_bytes / sizeof(uint64_t);
 		check_overflow(aligned_words);
 
 		// Reinterpret so we can fill unaligned bytes below.
-		uint8_t* buf = reinterpret_cast<uint8_t*>(_buf.get());
+		auto* buf = reinterpret_cast<uint8_t*>(_buf.get()); // NOLINT
 		std::memcpy(buf, ptr, size);
 		// It is safe to call this with 0 size.
 		// aligned >= size always.
-		std::memset(&buf[size], '\0', aligned_bytes - size);
+		std::memset(&buf[size], '\0', aligned_bytes - size); // NOLINT: Required p-arith.
 		_size += aligned_words;
 
 		return buf;
@@ -106,7 +106,7 @@ private:
 	uint64_t _size;
 	// Store our data as an array of uint64_t so we're (64-bit) word aligned
 	// by default.
-	std::unique_ptr<uint64_t[]> _buf;
+	std::unique_ptr<uint64_t[]> _buf; // NOLINT: Can't use std::array here.
 
 	// Align input value to 64-bits, i.e. 8 bytes.
 	static constexpr auto align64(uint64_t size) -> uint64_t
@@ -114,7 +114,7 @@ private:
 		// Anything not aligned will increase to above the next 8-byte
 		// aligned value, then by clearing the trailing bits we obtain
 		// that value. Aligned values remain the same.
-		return (size + 7) & ~7ULL;
+		return (size + (sizeof(uint64_t) - 1)) & ~(sizeof(uint64_t) - 1);
 	}
 
 	// Determine if we would overflow the capacity of the buffer and if so,
