682255b6f4de18e475030a22617b2f13c9351cf0
Lorenzo Stoakes <lstoakes@gmail.com>
2020-04-01 07:41:17 +0100

    dynamic_buffer: Add .add_raw() method to add arbitrary memory ranges to buffer
    The add_raw() method adds the ability to copy from any block of memory at a byte
    granularity which makes the dynamic buffer far more flexible and acts as the
    basis for a number of future intended uses (e.g. constructing a binary file
    format).
    
    The method copies the data into the buffer, filling any trailing unaligned bytes
    with zeroes.

--- include/dynamic_buffer.hh
+++ include/dynamic_buffer.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <cstring>
 #include <memory>
 #include <stdexcept>
 #include <string>
@@ -68,6 +69,28 @@ public:
 	{
 		check_overflow(1);
 		_buf[_size++] = n;
+	}
+
+	// Add raw data to the buffer. It will be aligned to word size with
+	// remaining bytes zeroed.
+	//      ptr: Raw pointer of memory to copy to buffer.
+	//     size: Size in bytes of memory to copy.
+	//  returns: Raw pointer to allocated memory.
+	void* add_raw(void* ptr, uint64_t size)
+	{
+		uint64_t aligned_bytes = align64(size);
+		uint64_t aligned_words = aligned_bytes / sizeof(uint64_t);
+		check_overflow(aligned_words);
+
+		// Reinterpret so we can fill unaligned bytes below.
+		uint8_t* buf = reinterpret_cast<uint8_t*>(_buf.get());
+		std::memcpy(buf, ptr, size);
+		// It is safe to call this with 0 size.
+		// aligned >= size always.
+		std::memset(&buf[size], '\0', aligned_bytes - size);
+		_size += aligned_words;
+
+		return buf;
 	}
 
 	// Clear the buffer but maintain the capacity.
