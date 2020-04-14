9334fb2817c775899c492f773c300d997d0f43c6
Lorenzo Stoakes <lstoakes@gmail.com>
2020-04-14 14:18:48 +0100

    update: Add skeleton for handling market definition and rc updates
    We will add implementation for these later, but this provides the skeleton of
    the code to handle the remainder of update data.

--- src/update.cc
+++ src/update.cc
@@ -31,6 +31,24 @@ static void check_op(const update_state& state, const sajson::value& root)
 					 op_str + "', expect 'mcm'");
 }
 
+// Parse market definition node and generate updates as required.
+static auto parse_market_definition(update_state& state, const sajson::value& market_def,
+				    dynamic_buffer& dyn_buf) -> uint64_t
+{
+	uint64_t num_updates = 0;
+
+	return num_updates;
+}
+
+// Parse runner change update.
+static auto parse_rc(update_state& state, const sajson::value& rc, dynamic_buffer& dyn_buf)
+	-> uint64_t
+{
+	uint64_t num_updates = 0;
+
+	return num_updates;
+}
+
 // Process market change update.
 static auto parse_mc(update_state& state, const sajson::value& mc, dynamic_buffer& dyn_buf)
 	-> uint64_t
@@ -66,6 +84,22 @@ static auto parse_mc(update_state& state, const sajson::value& mc, dynamic_buffe
 	if (traded_vol > 0) {
 		dyn_buf.add(make_market_traded_vol_update(traded_vol));
 		num_updates++;
+	}
+
+	// Handle market definition data.
+	sajson::value market_def = mc.get_value_of_key(sajson::literal("marketDefinition"));
+	num_updates += parse_market_definition(state, market_def, dyn_buf);
+
+	// Handle runner change updates.
+
+	sajson::value rcs = mc.get_value_of_key(sajson::literal("rc"));
+	if (rcs.get_type() != sajson::TYPE_ARRAY)
+		return num_updates;
+
+	uint64_t num_rcs = rcs.get_length();
+	for (uint64_t i = 0; i < num_rcs; i++) {
+		sajson::value rc = rcs.get_array_element(i);
+		num_updates += parse_rc(state, rc, dyn_buf);
 	}
 
 	return num_updates;
