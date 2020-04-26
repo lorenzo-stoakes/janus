f6839ab782f02d2819850d3b25874ae2afe401c3
Lorenzo Stoakes <lstoakes@gmail.com>
2020-04-26 18:27:40 +0100

    betfair: Add intitial betfair connection object
    This object brings together the logic I have been working on in order to allow
    network operations against the betfair API. It uses the [non-interactive][0]
    login using user-provided certificates which can be specified by the
    configuration file.
    
    It currently only implements login and logout but forms the basis for adding
    further functionality.
    
    [0]:https://docs.developer.betfair.com/display/1smk3cen4v3lu3yomq5qye0ni/Non-Interactive+%28bot%29+login

---
+++ include/network/betfair.hh
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "config.hh"
+#include "network/certs.hh"
+#include "network/client.hh"
+#include "network/http_request.hh"
+#include "network/rng.hh"
+
+namespace janus::betfair
+{
+// Login buffer size.
+static constexpr uint64_t DEFAULT_HTTP_BUF_SIZE = 1000;
+
+class connection
+{
+public:
+	connection(janus::tls::rng& rng, config& config)
+		: _certs_loaded{false}, _logged_in{false}, _rng{rng}, _config{config}
+	{
+	}
+
+	// Load certificates for betfair operations.
+	void load_certs();
+
+	// Login using non-interactive betfair login endpoint.
+	void login();
+
+	// Logout, invalidating session token.
+	void logout();
+
+	// Get current session token if logged in.
+	auto session_token() -> std::string
+	{
+		return _session_token;
+	}
+
+	static constexpr const char* ID_HOST = "identitysso-cert.betfair.com";
+	static constexpr const char* LOGIN_PATH = "/api/certlogin";
+	static constexpr const char* LOGOUT_PATH = "/api/logout";
+
+	static constexpr const char* API_HOST = "api.betfair.com";
+	static constexpr const char* API_PATH = "/exchange/betting/rest/v1.0";
+
+	static constexpr const char* STREAM_HOST = "stream-api.betfair.com";
+
+private:
+	// Maximum number of characters for either username or password.
+	static constexpr uint64_t MAX_USERNAME_PW_SIZE = 50;
+
+	// Everything uses the standard TLS port.
+	static constexpr const char* PORT = "443";
+
+	bool _certs_loaded;
+	bool _logged_in;
+	janus::tls::rng& _rng;
+	config& _config;
+	janus::tls::certs _certs, _self_sign_cert;
+
+	// We tolerate an allocate as logging in/out is slow anyway.
+	std::string _session_token;
+
+	// Assert that the certificates are loaded, throw otherwise.
+	void check_certs_loaded();
+
+	// Generate an HTTP request for logging in.
+	auto gen_login_req(char* buf, uint64_t cap) -> http_request;
+
+	// Generate an HTTP request for logging out.
+	auto gen_logout_req(char* buf, uint64_t cap) -> http_request;
+};
+} // namespace janus::betfair
