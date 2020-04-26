7373d5cd0b18983785cc5ba2ad3b619260361ac5
Lorenzo Stoakes <lstoakes@gmail.com>
2020-04-26 18:35:24 +0100

    jupiter: Update skeleton to connect to betfair
    Using jupiter as a skeleton program to explore use of betfair network tools,
    this initial version simply logs in to betfair, outputs the session token, then
    logs out.

--- src/jupiter/main.cc
+++ src/jupiter/main.cc
@@ -2,9 +2,21 @@
 
 #include <iostream>
 
+// This is a hacked up skeleton version for testing purposes, initially we are simply logging in to
+// betfair
+
 auto main(int argc, char** argv) -> int
 {
-	std::cout << "jupiter skeleton" << std::endl;
+	janus::tls::rng rng;
+	rng.seed();
+	janus::config config = janus::parse_config();
+
+	janus::betfair::session session(rng, config);
+	session.load_certs();
+
+	session.login();
+	std::cout << session.session_token() << std::endl;
+	session.logout();
 
 	return 0;
 }
