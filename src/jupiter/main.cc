afbc282ddef1924c53906a8d71dc4c0e942a663d
Lorenzo Stoakes <lstoakes@gmail.com>
2020-04-27 15:19:30 +0100

    jupiter: Get metadata and handle signals.

--- src/jupiter/main.cc
+++ src/jupiter/main.cc
@@ -1,15 +1,36 @@
 #include "janus.hh"
 
 #include "sajson.hh"
+#include <atomic>
 #include <iostream>
+#include <signal.h>
 #include <string>
 #include <vector>
 
 // This is a hacked up skeleton version for testing purposes, initially we are simply logging in to
 // betfair and performing simple queries.
 
+std::atomic<bool> interrupted{false};
+
+static void handle_signal(int)
+{
+	interrupted.store(true);
+}
+
+// Courtesy of https://stackoverflow.com/a/4250601
+static void add_signal_handler()
+{
+	struct sigaction sa;
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = handle_signal;
+	::sigfillset(&sa.sa_mask);
+	::sigaction(SIGINT, &sa, nullptr);
+}
+
 auto main(int argc, char** argv) -> int
 {
+	add_signal_handler();
+
 	janus::tls::rng rng;
 	rng.seed();
 
@@ -20,13 +41,22 @@ auto main(int argc, char** argv) -> int
 	session.login();
 
 	janus::betfair::stream stream(session);
-	stream.market_subscribe(config);
 
+	std::cout << "Getting metadata..." << std::endl;
+	std::string meta = stream.market_subscribe(config);
+	// Not doing anything with it yet.
+	(void)meta;
+
+	std::cout << "Done. Starting stream..." << std::endl;
 	while (true) {
+		if (interrupted.load())
+			break;
+
 		int size;
 		std::cout << stream.read_next_line(size) << std::endl;
 	}
 
+	std::cout << "Logging out..." << std::endl;
 	session.logout();
 
 	return 0;
