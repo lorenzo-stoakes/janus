dee8db74920bf62862f818d9e713586327e4d7a2
Lorenzo Stoakes <lstoakes@gmail.com>
2020-04-27 13:30:59 +0100

    jupiter: Update to use new overload.

--- src/jupiter/main.cc
+++ src/jupiter/main.cc
@@ -19,28 +19,17 @@ auto main(int argc, char** argv) -> int
 
 	session.login();
 
-	std::string filter_json =
-		R"({"eventTypeIds":["7"],"marketTypeCodes":["WIN"],"marketCountries":["US"]})";
+	std::string filter_json = R"({"eventTypeIds":["7"],"marketTypeCodes":["WIN"]})";
 
 	auto market_ids = janus::betfair::get_market_ids(session, filter_json);
 
 	// Now connect to stream API and output connection ID.
 	janus::betfair::stream stream(session);
-
 	std::cout << stream.connection_id() << std::endl;
-
-	std::string stream_filter_json = R"({"marketIds":[)";
-	for (uint64_t i = 0; i < market_ids.size() - 1; i++) {
-		stream_filter_json += "\"";
-		stream_filter_json += market_ids[i];
-		stream_filter_json += "\",";
-	}
-
-	stream_filter_json += "\"" + market_ids[market_ids.size() - 1] + "\"]}";
 
 	std::string data_filter_json =
 		R"({"ladderLevels": 10,"fields":["EX_ALL_OFFERS","EX_TRADED","EX_TRADED_VOL","EX_LTP","EX_MARKET_DEF"]})";
-	stream.market_subscribe(stream_filter_json, data_filter_json);
+	stream.market_subscribe(market_ids, data_filter_json);
 
 	while (true) {
 		int size;
