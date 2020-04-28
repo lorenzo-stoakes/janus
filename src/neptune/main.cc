6bb2837cc564e2daa35fd23b503466fa348b5f06
Lorenzo Stoakes <lstoakes@gmail.com>
2020-04-28 23:39:34 +0100

    neptune: Implement modern metadata retrieval
    Implement intended new approach for metadata retrieval using the new format
    metadata files being retrieved by jupiter.
    
    This is still incomplete as are not yet processing stream data but it does
    load/save state into a neptune.db file so we have the basis for the stream data
    implementation in place.

--- src/neptune/main.cc
+++ src/neptune/main.cc
@@ -1,214 +1,210 @@
 #include "janus.hh"
 
+#include "sajson.hh"
+#include "spdlog/spdlog.h"
+#include <algorithm>
+#include <cstdint>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <stdexcept>
 #include <string>
-#include <unordered_map>
+#include <vector>
+namespace fs = std::filesystem;
 
-//#define META
+static constexpr uint64_t MAX_METADATA_BYTES = 100'000;
 
-// A simple metadata -> binary tool for now so we can use this + a script to
-// extract legacy metadata to binary format.
+// Represents entry in neptune.db database file.
+struct db_entry
+{
+	db_entry() : file_id{0}, last_updated{0}, last_offset{0} {}
+	explicit db_entry(uint64_t id) : file_id{id}, last_updated{0}, last_offset{0} {}
 
-static constexpr uint64_t DYN_BUFFER_MAX_SIZE = 1'000'000'000;
-static constexpr uint64_t MARKET_DYN_BUFFER_MAX_SIZE = 100'000'000;
+	uint64_t file_id;
+	uint64_t last_updated;
+	uint64_t last_offset;
+};
 
+// Does the file at the specified path exist?
 static auto file_exists(std::string path) -> bool
 {
 	std::ifstream f(path);
 	return f.good();
 }
 
-auto parse_meta(const char* filename, janus::dynamic_buffer& dyn_buf) -> bool
+// Retrieve list of JSON files. By convention the filename is [ms since
+// epoch].js both for metadata and stream data.
+auto get_file_id_list(const janus::config& config) -> std::vector<uint64_t>
 {
-	std::string json;
-	uint64_t size;
+	std::vector<uint64_t> ret;
 
-	if (auto file = std::ifstream(filename, std::ios::ate)) {
-		size = file.tellg();
-
-		json = std::string(size, '\0');
-		file.seekg(0);
-
-		if (!file.read(&json[0], size)) {
-			std::cerr << "Couldn't read " << filename << std::endl;
-			return false;
-		}
-	} else {
-		std::cerr << "Couldn't open " << filename << std::endl;
-		return false;
+	std::string meta_dir_path = config.json_data_root + "/meta";
+	for (const auto& entry : fs::directory_iterator(meta_dir_path)) {
+		std::string num_str = entry.path().stem().string();
+		ret.push_back(std::stoll(num_str));
 	}
 
-	janus::betfair::parse_meta_json(filename, reinterpret_cast<char*>(json.data()), size,
-					dyn_buf);
-
-	return true;
+	// Since these are formated as ms since epoch, sorting will give us the
+	// files in correct chronological order.
+	std::sort(ret.begin(), ret.end());
+	return ret;
 }
 
-auto extract_by_market(janus::dynamic_buffer& dyn_buf, uint64_t num_updates)
-	-> std::unordered_map<uint64_t, std::vector<janus::update>>
+// Read database file containing information about imported data.
+auto read_db(const janus::config& config) -> std::unordered_map<uint64_t, db_entry>
 {
-	std::unordered_map<uint64_t, std::vector<janus::update>> map;
+	std::string path = config.binary_data_root + "/neptune.db";
 
-	if (num_updates < 2) {
-		std::cerr << "Only " << num_updates << " updates, expected minimum 2?" << std::endl;
-		throw std::runtime_error("Unexpected failure");
+	std::vector<db_entry> entries;
+	if (file_exists(path)) {
+		auto file = std::ifstream(path, std::ios::binary | std::ios::ate);
+		if (!file)
+			throw std::runtime_error(std::string("Cannot open ") + path);
+
+		uint64_t size = file.tellg();
+		entries = std::vector<db_entry>(size / sizeof(db_entry));
+
+		file.seekg(0);
+		file.read(reinterpret_cast<char*>(entries.data()), size);
 	}
 
-	// Read first update, HAS to be a timestamp.
-	janus::update& first = dyn_buf.read<janus::update>();
-	if (first.type != janus::update_type::TIMESTAMP) {
-		std::cerr << "First update is of type " << janus::update_type_str(first.type)
-			  << " not expected timestamp." << std::endl;
-		throw std::runtime_error("Unexpected failure");
-	}
-	uint64_t timestamp = janus::get_update_timestamp(first);
-
-	// Read second update, HAS to be a timestamp.
-	janus::update& second = dyn_buf.read<janus::update>();
-	if (second.type != janus::update_type::MARKET_ID) {
-		std::cerr << "Second update is of type " << janus::update_type_str(second.type)
-			  << " not expected market ID." << std::endl;
-		throw std::runtime_error("Unexpected failure");
-	}
-	uint64_t market_id = janus::get_update_market_id(second);
-
-	for (uint64_t i = 2; i < num_updates; i++) {
-		janus::update& u = dyn_buf.read<janus::update>();
-		if (u.type == janus::update_type::MARKET_ID) {
-			market_id = janus::get_update_market_id(u);
-
-			// When switching markets make sure we record the
-			// timestamp. This might result in duplicated timestamps
-			// but avoids missing ones.
-			map[market_id].push_back(janus::make_timestamp_update(timestamp));
-
-			continue;
-		}
-
-		if (u.type == janus::update_type::TIMESTAMP)
-			timestamp = janus::get_update_timestamp(u);
-
-		map[market_id].push_back(u);
+	// TODO(lorenzo): Probably not the most efficient method...
+	std::unordered_map<uint64_t, db_entry> map;
+	for (auto e : entries) {
+		map[e.file_id] = e;
 	}
 
 	return map;
 }
 
-auto output_stream(janus::dynamic_buffer& dyn_buf, uint64_t num_updates, std::string destdirname)
-	-> bool
+// Write database file back.
+void write_db(const janus::config& config, const std::unordered_map<uint64_t, db_entry>& map)
 {
-	try {
-		auto map = extract_by_market(dyn_buf, num_updates);
-
-		for (auto [market_id, updates] : map) {
-			std::string path = destdirname + "/" + std::to_string(market_id) + ".jan";
-
-			if (file_exists(path))
-				std::cerr << "Stream output file " << path
-					  << " already exists, appending." << std::endl;
-
-			if (auto file = std::ofstream(path, std::ios::binary | std::ios::app)) {
-				file.write(reinterpret_cast<char*>(updates.data()),
-					   updates.size() * sizeof(janus::update));
-			} else {
-				std::cerr << "Cannot open " << path << " for writing?" << std::endl;
-				return false;
-			}
-		}
-
-	} catch (std::exception& e) {
-		std::cerr << "ERROR: " << e.what() << std::endl;
-		return false;
+	std::vector<db_entry> entries;
+	for (auto [_, e] : map) {
+		entries.push_back(e);
 	}
 
-	return true;
+	std::sort(entries.begin(), entries.end(),
+		  [](db_entry& e1, db_entry& e2) { return e1.file_id < e2.file_id; });
+
+	std::string path = config.binary_data_root + "/neptune.db";
+	auto file = std::ofstream(path, std::ios::binary);
+	if (!file)
+		throw std::runtime_error(std::string("Can't open DB ") + path + " for writing.");
+
+	file.write(reinterpret_cast<char*>(entries.data()), entries.size() * sizeof(db_entry));
 }
 
-auto parse_stream(janus::dynamic_buffer& dyn_buf, const char* filename, uint64_t& num_updates)
-	-> bool
+// Write metadata file.
+void write_meta(const std::string& dest_dir, janus::dynamic_buffer& dyn_buf)
 {
-	auto file = std::ifstream(filename);
-	if (!file) {
-		std::cerr << "Couldn't open " << filename << std::endl;
-		return false;
+	auto& header = dyn_buf.read<janus::meta_header>();
+	uint64_t market_id = header.market_id;
+
+	std::string path = dest_dir + "/" + std::to_string(market_id) + ".jan";
+
+	auto file = std::ofstream(path, std::ios::binary);
+	if (!file)
+		throw std::runtime_error(std::string("Can't open ") + path + " for writing.");
+
+	file.write(reinterpret_cast<char*>(dyn_buf.data()), dyn_buf.size());
+}
+
+// Parse metadata line from aggregated metadata file. Returns number of markets parsed.
+auto parse_meta_line(const std::string& dest_dir, std::string& line) -> uint64_t
+{
+	sajson::document doc =
+		janus::internal::parse_json("", reinterpret_cast<char*>(line.data()), line.size());
+	const sajson::value& root = doc.get_root();
+
+	if (root.get_type() != sajson::TYPE_ARRAY)
+		throw std::runtime_error("Unexpected metadata root JSON type?");
+
+	janus::dynamic_buffer dyn_buf(MAX_METADATA_BYTES);
+
+	uint64_t count = root.get_length();
+	for (uint64_t i = 0; i < count; i++) {
+		sajson::value el = root.get_array_element(i);
+
+		janus::betfair::parse_meta_json(el, dyn_buf);
+		write_meta(dest_dir, dyn_buf);
+		dyn_buf.reset();
 	}
 
-	num_updates = 0;
+	return count;
+}
 
-	janus::betfair::price_range range;
-	janus::betfair::update_state state = {
-		.range = &range,
-		.filename = filename,
-		.line = 1,
-	};
+// Parse metadata and save binary representation.
+void parse_meta(janus::config& config, uint64_t file_id)
+{
+	std::string source_path =
+		config.json_data_root + "/meta/" + std::to_string(file_id) + ".json";
+	if (!file_exists(source_path))
+		throw std::runtime_error(std::string("Cannot find metadata JSON file ") +
+					 source_path);
 
+	spdlog::info("Found new metadata file {}", source_path);
+
+	std::string dest_dir = config.binary_data_root + "/meta/";
+	if (!file_exists(dest_dir))
+		throw std::runtime_error(std::string("Cannot find destination directory ") +
+					 dest_dir);
+
+	auto file = std::ifstream(source_path);
+	if (!file)
+		throw std::runtime_error(std::string("Cannot open file ") + source_path);
+
+	spdlog::info("Writing metadata...");
+	uint64_t num_markets = 0;
 	std::string line;
 	while (std::getline(file, line)) {
 		if (line.empty() || line[0] == '\0')
 			continue;
 
-		num_updates += janus::betfair::parse_update_stream_json(
-			state, reinterpret_cast<char*>(line.data()), line.size(), dyn_buf);
+		num_markets += parse_meta_line(dest_dir, line);
+	}
+	spdlog::info("Wrote {} markets.", num_markets);
+}
+
+// Run core program loop.
+auto run_loop(janus::config& config) -> bool
+{
+	spdlog::info("Reading DB file...");
+	auto map = read_db(config);
+
+	auto file_ids = get_file_id_list(config);
+	spdlog::info("Found {} metadata files.", file_ids.size());
+	spdlog::info("Checking for new metadata...");
+	// Add entries for new files.
+	for (uint64_t file_id : file_ids) {
+		if (!map.contains(file_id)) {
+			// If we haven't seen the file before, parse the
+			// metadata and save it to the binary metadata
+			// directory.
+			parse_meta(config, file_id);
+			map.emplace(file_id, file_id);
+		}
 	}
 
-	std::cout << num_updates << std::endl;
+	spdlog::info("Writing DB file...");
+	write_db(config, map);
 
+	spdlog::info("Done!");
 	return true;
 }
 
-auto main(int argc, char** argv) -> int
+auto main() -> int
 {
-	if (argc != 3) {
-		std::cerr << "Usage: " << argv[0] << " [source file] [destination file]"
-			  << std::endl;
-		return 1;
+	janus::config config = janus::parse_config();
+
+	bool success;
+	try {
+		success = run_loop(config);
+	} catch (std::exception& e) {
+		spdlog::error(e.what());
+		spdlog::critical("Aborting!");
+		success = false;
 	}
 
-	std::string source = argv[1];
-	std::string dest = argv[2];
-
-	janus::dynamic_buffer dyn_buf(DYN_BUFFER_MAX_SIZE);
-
-#ifdef META
-	if (file_exists(dest)) {
-		std::cerr << "Destination file " << dest << " already exists?" << std::endl;
-		return 1;
-	}
-
-	if (!parse_meta(source.c_str(), dyn_buf)) {
-		std::cerr << "Couldn't parse metadata from " << source << std::endl;
-		return 1;
-	}
-
-	if (auto file = std::ofstream(dest, std::ios::binary)) {
-		file.write(reinterpret_cast<char*>(dyn_buf.data()), dyn_buf.size());
-	} else {
-		std::cerr << "Couldn't open " + dest + " for writing" << std::endl;
-		return 1;
-	}
-#else
-	if (!file_exists(dest)) {
-		std::cerr << "Destination directory " << dest << " doesn't exist?" << std::endl;
-		return 1;
-	}
-
-	uint64_t num_updates = 0;
-
-	if (!parse_stream(dyn_buf, source.c_str(), num_updates)) {
-		std::cerr << "Couldn't parse stream data from " << source << std::endl;
-		return 1;
-	}
-
-	// In the case of stream data the destination specifies the directory to
-	// write data files into.
-	if (!output_stream(dyn_buf, num_updates, dest)) {
-		std::cerr << "Couldn't output stream data from " << source << " into directory "
-			  << dest << std::endl;
-		return 1;
-	}
-#endif
-
-	return 0;
+	return success ? 0 : 1;
 }
